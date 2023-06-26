62cbc249cf874085bd0b65fd753dac953f2a84a7
Azhar-A-Shaikh <117568791+Azhar-A-Shaikh@users.noreply.github.com>
2023-06-26 16:51:59 +0530

    Update and rename Spark Batch Processing using AWS (EMR, EC2, GLUE, Athena ) /EMR/csv_to_parquet.py to Spark Batch Processing using AWS (EMR, EC2, GLUE, Athena )

--- Spark Batch Processing using AWS (EMR, EC2, GLUE, Athena ) /EMR/csv_to_parquet.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("souce process").getOrCreate()
-
-source_df = spark.read.csv("s3://pysparkapi/banktxn/csv/banktxn.csv", header=True)
- 
-source_df.write.parquet("s3://pysparkapi/banktxn/parquet") # to write into parquet format.
